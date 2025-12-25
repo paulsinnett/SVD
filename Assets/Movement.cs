86ed98fcdd95e05c994a130af852167f2acf0d80
Paul Sinnett <paul.sinnett@gmail.com>
2025-12-25 23:13:44 +0000

    add movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using Unity.Mathematics;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 5;
+    public InputActionReference move;
+
+    public void OnEnable()
     {
-        
+        move.action.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDisable()
     {
+        move.action.Disable();
+    }
+
+    public void Update()
+    {
+        Vector2 movement = move.action.ReadValue<Vector2>();
+        transform.Translate(speed * Time.deltaTime * new float3(movement.x, 0, movement.y), Space.World);
         
     }
 }
